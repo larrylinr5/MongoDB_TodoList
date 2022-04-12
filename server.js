5bcb1646443ffe53b7f8d0355491532be55c0173
Larry <larrylinr5@gmail.com>
2022-04-12 20:01:14 +0800

    新增 => server.js 載入 libs.js 並加入 api 控制

--- server.js
+++ server.js
@@ -2,32 +2,31 @@ const http = require("http");
 /** 載入 mongoose 套件 */
 const mongoose = require('mongoose');
 /** 載入 全域變數套件 */
-const dotenv = require('dotenv')
-
+const dotenv = require('dotenv');
 // 全域變數套件設定
 dotenv.config({ path: "./config.env" })
+// 資源庫
+const libs = require('./libs');
 
 //#region 連接資料庫
-// 本地連線
-//mongoose.connect('mongodb://localhost:27017/hotel')
-
-// 遠端連線
+// 遠端連線字串
 const connectString = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
 )
-
+// 遠端連線
 mongoose.connect(connectString)
-  .then(() => {
-    console.log('資料庫連線成功')
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 //#endregion
 
 const requestListener = (req, res) => {
-  console.log('hello')
+  const { headers, message } = libs
+  
+  if (method === "OPTIONS") {
+    res.writeHead(200, headers)
+    res.end()
+  } else {
+    errorHandler(res, 404, message[404])
+  }
 }
 
 const server = http.createServer(requestListener);
