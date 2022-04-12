d0066a4fb79540024b7ebf93dc237fd0469b5cd8
Larry <larrylinr5@gmail.com>
2022-04-12 20:22:08 +0800

    新增 => server.js ( 完善程式碼 )

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//#region 功能載入區塊
 const http = require("http");
 /** 載入 mongoose 套件 */
 const mongoose = require('mongoose');
@@ -9,6 +10,15 @@ dotenv.config({ path: "./config.env" })
 const libs = require('./libs');
 // 回應控制
 const { errorHandler } = require('./responseHandler');
+// 讀取功能 api
+const { getTodo, getTodos } = require('./getTodo');
+// 新增功能 api
+const postTodo = require('./postTodo');
+// 刪除功能 api
+const { deleteTodos, deleteTodo } = require('./deleteTodo');
+// 單筆修改功能 api
+const patchTodo = require('./patchTodo');
+//#endregion
 
 //#region 連接資料庫
 // 遠端連線字串
@@ -16,14 +26,34 @@ const connectString = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
 )
-// 遠端連線
+// 連線字串
 mongoose.connect(connectString)
 //#endregion
 
-const requestListener = (req, res) => {
+const requestListener = async (req, res) => {
   const { headers, message } = libs
-  
-  if (method === "OPTIONS") {
+  const { url, method } = req
+  /** requestListener 資訊與清單物件 */
+  const data = {
+    /** requestListener req */
+    req,
+    /** requestListener res */
+    res
+  }
+
+  if (url === "/todos" && method === "GET") {
+    getTodos(data)
+  } else if (url.startsWith("/todos/") && method === "GET") {
+    getTodo(data)
+  } else if (url === "/todos" && method === "POST") {
+    postTodo(data)
+  } else if (url === "/todos" && method === "DELETE") {
+    deleteTodos(data)
+  } else if (url.startsWith("/todos/") && method === "DELETE") {
+    deleteTodo(data)
+  } else if (url.startsWith("/todos/") && method === "PATCH") {
+    patchTodo(data)
+  } else if (method === "OPTIONS") {
     res.writeHead(200, headers)
     res.end()
   } else {
